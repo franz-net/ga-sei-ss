3c6113e3d11f71aba77018aa7800d9dc26a50921
Franz <5334409+franz-net@users.noreply.github.com>
2022-04-16 01:58:39 -0500

    added login functionality

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,9 +1,10 @@
 import {BadRequestError} from "../errors/index"
 import User from "../models/User";
 import {StatusCodes} from "http-status-codes";
+import {UnauthenticatedError} from "../errors";
 
-const register = async (req, res, next) => {
-    
+const register = async (req, res) => {
+
     const {name, email, password} = req.body
 
     if (!name || !email || !password) {
@@ -27,4 +28,23 @@ const register = async (req, res, next) => {
     })
 }
 
-export {register}
+const login = async (req, res) => {
+    const {email, password} = req.body
+    if (!email || !password) {
+        throw new BadRequestError('Please provide all values')
+    }
+    const user = await User.findOne({email}).select('+password')
+    if (!user) {
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+    const isPasswordCorrect = await user.comparePassword(password)
+    if (!isPasswordCorrect) {
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    const token = user.createJWT()
+    user.password = undefined
+    res.status(StatusCodes.OK).json({user, token})
+}
+
+export {register, login}
--- server/routes/authRoute.ts
+++ server/routes/authRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import {register} from "../controllers/authController";
+import {login, register} from "../controllers/authController";
 
 const authRouter = express.Router()
 
 authRouter.route('/register').post(register)
+authRouter.route('/login').post(login)
 
 
 export default authRouter
