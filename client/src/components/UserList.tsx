7143bc85518b8e59289a2e211c42033557da2b7f
Franz <5334409+franz-net@users.noreply.github.com>
2022-06-21 23:04:34 -0500

    datagrid for users

--- client/src/components/UserList.tsx
+++ client/src/components/UserList.tsx
@@ -1,7 +1,16 @@
 import {useAppContext} from "../context/appContext";
 import {useEffect} from "react";
 import {Box, Paper, Typography} from "@mui/material";
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import {Loader} from "./index";
+
+const columns: GridColDef[] = [
+    {field: 'email', headerName: 'e-mail', width: 130},
+    {field: 'name', headerName: 'First Name', width: 130},
+    {field: 'lastName', headerName: 'Last Name', width: 130},
+    {field: 'role', headerName: 'Role', width: 130, sortable: true},
+    
+]
 
 export default function UserList() {
     const {getUserList, totalUsers, userList, isLoading} = useAppContext()
@@ -29,18 +38,18 @@ export default function UserList() {
             <Typography color='secondary' sx={{ml: 1}}>
                 {totalUsers} user {userList.length > 1 && 's'} found
             </Typography>
-            <Box component='div' sx={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                justifyContent: 'center'
-            }}>
-                {userList.map((user: any, index: number) => {
-                        return (
-                            <h1 key={index}>{user.name}</h1>
-                        )
-                    }
-                )
-                }
+            <Box component='div' 
+                sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent: 'center',
+                    height: '400px'
+                }}
+            >
+                <DataGrid
+                    columns={columns}
+                    rows={userList}
+                />
             </Box>
         </>
     )
